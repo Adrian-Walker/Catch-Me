10b7d85b3282d8fc03b62ce653f560fb19a4d87f
Adrian Walker <awalker3365@gmail.com>
2021-04-25 22:26:54 -0400

    2nd

--- catchme.js
+++ catchme.js
@@ -1,3 +1,29 @@
-function sum(x,y){
-    return x + y
+// 1.)
+try {
+    function sum(x, y) {
+        return 1 + 2
+    }
+    throw "This is it!"
+}
+catch (err) {
+    console.log("error")
+}
+
+// 2.)*
+
+var login = {
+    Username: "NFL Fan",
+    Password: "5588",
+};
+try {
+    function user(Username, Password) {
+        if(login = true){
+            throw "Login Successful"
+        }
+    }
+
+}
+
+catch {
+    console.log("Incorrect Login Information")
 }
